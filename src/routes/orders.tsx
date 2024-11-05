1c4abd994d52e3a751c801338e00e77cc94daa8e
Myguel Angello <sih@saocamilofortaleza.org.br>
2024-11-05 17:13:20 -0300

    feat: Add ApproveOrderDialog component and integrate it into the Order route

--- src/routes/orders.tsx
+++ src/routes/orders.tsx
@@ -1,3 +1,4 @@
+import { ApproveOrderDialog } from "@/components/approve-order-order";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -14,43 +15,46 @@ export function Order() {
 
 
     return (
-        <div className="flex flex-col gap-6 px-4">
-            <header className="w-full mt-9 flex items-center justify-center">
-                <div className="flex items-center justify-start w-1/3">
+        <div className="flex flex-col gap-6 pb-5 justify-start h-screen overflow-x-hidden bg-background">
+
+
+            <header className="fixed top-0 left-0 w-full flex items-center justify-center bg-background z-10 pt-5 pb-2">
+                <div className="flex items-center justify-start w-1/3 pl-3">
                     <ArrowLeft className="h-6 w-6 text-zinc-600" />
                 </div>
-
                 <div className="flex items-center justify-center w-1/3">
                     <h3 className="text-xl font-bold antialiased text-zinc-600">Ordem {order?.number}</h3>
                 </div>
-
                 <div className="w-1/3"></div>
             </header>
 
-            <main className="flex flex-col gap-3">
-                <Input value={order?.requester} className="w-full bg-zinc-300" readOnly disabled />
+            <div className="px-4 mt-16 pt-5 shadow-inner" style={{ height: 'calc(100vh - 80px)', overflowY: 'scroll' }}>
+                <main className="flex flex-col gap-3">
+                    <Input value={order?.requester} className="w-full bg-zinc-100" readOnly disabled />
 
-                <div className="flex w-full">
-                    <div className="mr-2 w-full">
-                        <label className="text-lg text-zinc-600" htmlFor="date_occurrence">Data de ocorrência</label>
-                        <Input value={order?.date_occurrence} className="w-full" id="date_occurrence" readOnly disabled />
+                    <div className="flex w-full">
+                        <div className="mr-2 w-full">
+                            <label className="text-lg text-zinc-600" htmlFor="date_occurrence">Data de ocorrência</label>
+                            <Input value={order?.date_occurrence} className="w-full" id="date_occurrence" readOnly disabled />
+                        </div>
+
+                        <div className="w-full">
+                            <label className="text-lg text-zinc-600" htmlFor="date_order">Data da ordem</label>
+                            <Input value={order?.date_order} className="w-full bg-zinc-100" id="date_order" readOnly disabled />
+                        </div>
                     </div>
 
                     <div className="w-full">
-                        <label className="text-lg text-zinc-600" htmlFor="date_order">Data da ordem</label>
-                        <Input value={order?.date_order} className="w-full" id="date_order" readOnly disabled />
+                        <label className="text-lg text-zinc-600" htmlFor="date_order">Descrição</label>
+                        <Textarea value={order?.describe} className="h-min w-full bg-zinc-100" rows={10} readOnly disabled />
                     </div>
-                </div>
+                </main>
+            </div>
 
-                <div className="w-full">
-                    <label className="text-lg text-zinc-600" htmlFor="date_order">Descrição</label>
-                    <Textarea value={order?.describe} rows={8} readOnly disabled />
-                </div>
-            </main>
+            <footer className="fixed bottom-0 w-full p-5 flex flex-col gap-2 bg-background">
+                <ApproveOrderDialog />
 
-            <footer>
-                <Button className="w-full bg-emerald-600 hover:bg-emerald-500 mb-2" variant="default">Aprovar</Button>
-                <Button className="w-full bg-red-600 hover:bg-red-500">Reprovar</Button>
+                <Button className="w-full h-14 bg-red-500 hover:bg-red-600">Reprovar</Button>
             </footer>
         </div>
     )
