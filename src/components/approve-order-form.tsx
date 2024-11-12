ee9b20a9bdc0139ca1ffbc1f47ff963996ba9967
Myguel Angello <sih@saocamilofortaleza.org.br>
2024-11-12 14:59:05 -0300

    feat: Refactor approve and disapprove order forms to use React Hook Form and Zod for validation; improve user experience with loading states

--- src/components/approve-order-form.tsx
+++ src/components/approve-order-form.tsx
@@ -1,63 +1,89 @@
 import api from "@/lib/axios"
 import { cn } from "@/lib/utils"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { AxiosError } from "axios"
 import Cookies from "js-cookie"
-import { ComponentProps, useState } from "react"
+import { Loader2 } from "lucide-react"
+import { ComponentProps } from "react"
+import { useForm } from "react-hook-form"
+import { useNavigate } from "react-router-dom"
 import { toast } from "sonner"
+import { z } from "zod"
 import { Button } from "./ui/button"
 import { Label } from "./ui/label"
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group"
 import { Textarea } from "./ui/textarea"
 
+const orderApprovalSchema = z.object({
+    observation: z.string({ message: "Campo obrigatório" }),
+    treatment: z.string({ message: "Campo obrigatório" })
+})
+type OrderApprovalData = z.infer<typeof orderApprovalSchema>
+
 type ApproveOrderFormProps = ComponentProps<"form"> & { approve: boolean, orderId: number }
 
 enum TreatmentOptions {
     Banco = "Utilizar banco de horas",
-    Abonar = "Abonar justificativa",
-    Descontar = "Descontar em folha"
+    abonar = "Abonar",
+    descontar = "Descontar em folha"
 }
 
 export function ApproveOrderForm({ className, approve, orderId }: ApproveOrderFormProps) {
-    const [formData, setFormData] = useState({
-        observation: "",
-        treatment: ""
+    const queryClient = useQueryClient()
+    const navigate = useNavigate()
+
+    const { register, handleSubmit, formState: { errors, isSubmitting }, setValue } = useForm<OrderApprovalData>({
+        resolver: zodResolver(orderApprovalSchema),
     })
 
-    const handleRadioChange = (value: string) => {
-        setFormData(prev => ({ ...prev, treatment: value }))
-        console.log(value)
-    }
-    const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setFormData(prev => ({ ...prev, observation: e.target.value }))
-    }
+    const { mutateAsync } = useMutation({
+        mutationFn: async (data: OrderApprovalData) => handleSendApprove(data),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["justification-pendents"] })
+        }
+    })
 
-    async function handleSendApprove(e: React.FormEvent) {
-        e.preventDefault()
+    async function handleSendApprove({ observation, treatment }: OrderApprovalData) {
         try {
             const user = Cookies.get("j.ai.user")
             const data = {
                 user: user,
                 order: orderId,
-                treatment: TreatmentOptions[formData.treatment as keyof typeof TreatmentOptions],
-                observation: formData.observation,
+                treatment: TreatmentOptions[treatment as keyof typeof TreatmentOptions],
+                observation: observation,
                 approve
             }
             const response = await api.post("/justification/manager/action", data)
             console.log(response)
             toast.info("Aprovação enviada com sucesso")
-        }
-        catch (error) {
-            console.error(error)
-            toast.error("Erro ao enviar aprovação, tente novamente")
+
+            navigate("/manager")
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                return toast.error(error.response?.data.message)
+            }
+            return toast.error("Erro ao enviar aprovação, tente novamente")
         }
     }
+
+    async function submitApprove({ observation, treatment }: OrderApprovalData) {
+        await mutateAsync({ observation, treatment })
+    }
+
+    const handleRadioChange = (value: string) => {
+        setValue("treatment", value)
+        console.log(value)
+    }
+
     return (
-        <form onSubmit={handleSendApprove} className={cn("grid items-start gap-4", className)}>
+        <form onSubmit={handleSubmit(submitApprove)} className={cn("grid items-start gap-4", className)}>
             <div className="grid gap-2">
-                <Label htmlFor="apreciacao">Apreciação</Label>
+                <Label htmlFor="treatment">Apreciação</Label>
                 <RadioGroup
                     onValueChange={handleRadioChange}
                     required
-                    id="apreciacao"
+                    id="treatment"
                 >
                     <div className="flex items-center space-x-2">
                         <RadioGroupItem value="abonar" id="abonar" />
@@ -71,22 +97,29 @@ export function ApproveOrderForm({ className, approve, orderId }: ApproveOrderFo
                         <RadioGroupItem value="descontar" id="descontar-folha" />
                         <Label htmlFor="descontar-folha">Descontar em folha</Label>
                     </div>
+                    {errors.treatment && (
+                        <span className="text-red-500">{errors?.treatment?.message}</span>
+                    )}
                 </RadioGroup>
             </div>
             <div className="grid gap-2">
-                <Label htmlFor="complement">Comentário</Label>
+                <Label htmlFor="observation">Comentário</Label>
                 <Textarea
-                    id="complement"
-                    name="complement"
+                    id="observation"
                     placeholder=""
-                    value={formData.observation}
-                    onChange={handleTextareaChange}
+                    {...register("observation")}
                     className="w-full"
                     required
                     rows={5}
                 />
+                {errors.observation && (
+                    <span className="text-red-500">{errors?.observation?.message}</span>
+                )}
             </div>
-            <Button type="submit" className="bg-emerald-600 hover:bg-emerald-500">Confirmar aprovação</Button>
+
+            <Button disabled={isSubmitting} type="submit" className="bg-emerald-600 hover:bg-emerald-500">
+                {isSubmitting ? <Loader2 className="size-3 animate-spin" /> : "Confirmar aprovação"}
+            </Button>
         </form>
     )
 }
