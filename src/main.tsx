ea2cccc964fe3c1981800a0f8bc8a2f2d608ec72
Myguel Angello <sih@saocamilofortaleza.org.br>
2024-11-21 17:06:49 -0300

    chore: Update dependencies for React Query and add React Query Devtools for enhanced debugging

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Toaster position='top-center' richColors />
       <AuthContextProvider>
         <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
       </AuthContextProvider>
     </QueryClientProvider>
   </React.StrictMode>,
