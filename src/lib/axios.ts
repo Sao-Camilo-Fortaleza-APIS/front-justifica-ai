5250d4dc8ff60b90ad03ca1799ec9ae7e5d519f6
Myguel Angello <sih@saocamilofortaleza.org.br>
2024-12-06 15:52:48 -0300

    feat: Add response interceptor to handle 401 errors and clear authentication cookies

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,20 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 
 const api = axios.create({
     baseURL: `${import.meta.env.VITE_BASE_URL}`
 });
+
+api.interceptors.response.use((response) => {
+    return response
+}, (error) => {
+    if (error.response.status === 401) {
+        Cookies.remove('j.ai.token')
+        Cookies.remove('j.ai.user')
+        window.location.href = '/'
+    }
+    return Promise.reject(error)
+})
 
 export default api;
